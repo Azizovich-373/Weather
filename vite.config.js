d395871db5683e906433b6b26339c3ce2c4fa861
SAMIR GANIYEV <samirganiev777@gmail.com>
2024-08-15 11:37:15 +0500

    fixed pages

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import {
+    resolve
+} from 'path'
+import {
+    defineConfig
+} from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                days: resolve(__dirname, 'pages/days/index.html'),
+            },
+        },
+    },
+})
